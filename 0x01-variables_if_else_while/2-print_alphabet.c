1502d8250f38d91c68464a7750d8aaa9ef6d3d56
JULESROWLAND <juliettrowland@gmail.com>
2023-07-17 16:48:51 -0700

    When using loop skip the semi colo

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Return: 0 Always (success)
+ * main - Entry point for all c file
+ * Return: 0 Always (Success)
  */
 int main(void)
 {
-	char letter[] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	 char let[] = "abcdefghijklmnopqrstuvwxyz";
+	 int i;
 
-	for (i = 0; i <= 25; i++)
-		{
-			putchar(letter[i]);
-		}
+	for (i = 0, i <= 0, i++)
+	{
+		putchar(let[i]);
+	}
 	putchar('\n');
 	return (0);
 }
