359da9dd98ef9949102cdc9503b5acbbf9bbd38d
JULESROWLAND <juliettrowland@gmail.com>
2023-07-19 20:13:00 -0700

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+/**
+ * _abs - Check for absolute interger
+ * Return: 0 always successful
+ * @y: This creates data based on certain type
+ */
+int _abs(int y)
+{
+	if (y > 0)
+	{
+	return (y);
+	}
+	return (-y);
+}
