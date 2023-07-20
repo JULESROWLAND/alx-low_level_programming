e46bb383bd91fe70cdf61c3ea70f83ccec08c139
JULESROWLAND <juliettrowland@gmail.com>
2023-07-20 16:41:28 -0700

    Debug

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+void positive_or_negative(int n)
+{
+    if (n > 0)
+        printf("%d is positive\n", n);
+    else if (n < 0)
+        printf("%d is negative\n", n);
+    else
+        printf("0 is zero\n");
+}
