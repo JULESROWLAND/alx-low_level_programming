b64a2881c6b15c26841537133923d5fa690b3435
JULESROWLAND <juliettrowland@gmail.com>
2023-07-21 14:36:28 -0700

    This prints a char variables

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - This prints a series of numbers
+ * Return: This return 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	_putchar('0' + i);
+	}
+	_putchar('\n');
+}
