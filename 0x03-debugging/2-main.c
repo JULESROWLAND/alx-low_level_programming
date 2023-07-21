631fa475fbd6c3566decc47bb6ed92ac67000b0d
JULESROWLAND <juliettrowland@gmail.com>
2023-07-20 18:49:17 -0700

    Conditions

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -4,6 +4,9 @@
 /**
 * main - prints the largest of 3 integers
 * Return: 0
+* @a: interger value
+* @b: interger value
+* @c: interfer value
 */
 
 int largest_number(int a, int b, int c);
@@ -12,13 +15,13 @@ int main(void)
 	int a, b, c;
 	int largest;
 
-        a = 972;
+	a = 972;
 	b = -98;
 	c = 0;
 
 	largest = largest_number(a, b, c);
 
-        printf("%d is the largest number\n", largest);
+	printf("%d is the largest number\n", largest);
 
-        return (0);
+	return (0);
 }
