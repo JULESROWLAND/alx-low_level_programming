3a8f0031ad7491f9351b3f272b18e5284d25749a
JULESROWLAND <juliettrowland@gmail.com>
2023-07-21 13:57:25 -0700

    This multiples two intergers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+ * mul - This multiples to numbers
+ * Return: this returns the result of our multiiplication
+ * @a: This is  an Interger value
+ * @b: This is an Interger value
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
