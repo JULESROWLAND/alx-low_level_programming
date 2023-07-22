beb1d3fe27b7b67510899966d1dad374930fe49b
JULESROWLAND <juliettrowland@gmail.com>
2023-07-22 16:58:19 -0700

    random no

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+	char let[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i <= 0; i++)
+		{
+			putchar(let[50]);
+		}
+	putchar('\n');
+	return (0);
+}
