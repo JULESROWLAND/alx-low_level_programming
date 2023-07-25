954621130c0dde196fc597a0553d777f5d4bd02b
JULESROWLAND <juliettrowland@gmail.com>
2023-07-24 18:55:03 -0700

    This is how we check for length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - This check for the length of the given string
+ * @s: This collects a string pointer
+ * Return: a value.
+ */
+int _strlen(char *s)
+{
+	int length = strlen(s);
+
+	return (length);
+}
