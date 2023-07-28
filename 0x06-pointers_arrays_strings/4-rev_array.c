f0c4e8ee565b7dcb808e670603a26f7db9637cab
JULESROWLAND <juliettrowland@gmail.com>
2023-07-28 11:43:26 -0700

    This is how to reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,17 +2,19 @@
 
 /**
  * reverse_array - This function reverses the array of items
- * @*a: This is a pointer variable
+ * @a: This is a pointer variable
  * @n: This takes an interger
  */
 void reverse_array(int *a, int n)
 {
-	int i,space;
+	int i, space;
 
-	for (i = 0; i < n--; i++)
+	i = 0;
+	while (i < n--)
 	{
 	space = a[i];
 	a[i] = a[n];
 	a[n] = space;
+	i++;
 	}
 }
