64b061ee5dbd346b680dafdc765ef0bd32d7451d
JULESROWLAND <juliettrowland@gmail.com>
2023-07-31 16:30:17 -0700

    This is how we set values

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - fill a block of memory with a specific value.
+ * @s: This points to a variable array
+ * @b: This takes a character argument
+ * @n: This takes an interger array
+ * Return: This returns a pointer.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
