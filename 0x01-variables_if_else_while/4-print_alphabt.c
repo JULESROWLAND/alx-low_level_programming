951a27670059be51390f11cf74e5d0423d8c9fb5
JULESROWLAND <juliettrowland@gmail.com>
2023-08-06 13:33:20 -0700

    Working with if else

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This is the Entry Point
+ * Return: this return 0.
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		if (a == 101 || a == 113)
+			continue;
+		putchar(a);
+	}
+	putchar(a);
+	return (0);
+}
