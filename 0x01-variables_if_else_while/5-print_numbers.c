82b6f45eb9f3e1a74d603d079e61e8272205e7e6
JULESROWLAND <juliettrowland@gmail.com>
2023-08-06 13:50:16 -0700

    This is how we print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - This ia an Entry Point
+ * Return: This returns the value of 0.
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d", a);
+	}
+	return (0);
+}
