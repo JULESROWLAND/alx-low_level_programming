fc46f4d618dc059cc371bf5cdd9578a6d59464ed
JULESROWLAND <juliettrowland@gmail.com>
2023-08-06 14:07:33 -0700

    This prints a number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: This returns 0.
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
