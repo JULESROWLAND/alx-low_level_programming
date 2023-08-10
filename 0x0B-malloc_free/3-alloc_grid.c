f3629fbbd90493e17759600d6a888ebdd23a077d
JULESROWLAND <juliettrowland@gmail.com>
2023-08-09 21:58:46 -0700

    Memory allocation for 2D array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,20 +16,31 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	grid = (int **) malloc(width * sizeof(int *));
+	grid = (int **) malloc(height * sizeof(int *));
 
 	if (grid == 0)
 	{
 	return (NULL);
 	}
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-	grid[i] = (int *) malloc(height * sizeof(int));
+	grid[i] = (int *) malloc(width * sizeof(int));
+	if(grid[i] == NULL)
+	{
+		for (i = 0; i >= 0; i++)
+	{
+	free (grid[i]);
+	return (NULL);
+
+	free(grid);
+	return(NULL);
 	}
-	for (i = 0; i < width; i++)
+	}
+	}
+	for (i = 0; i < height; i++)
 	{
-	for (j = 0; j < height; j++)
+	for (j = 0; j < width; j++)
 	{
 	grid[i][j] = 0;
 	}
