fc442831b0e9a97e3e7b980d4e437a87ba8ffe9f
JULESROWLAND <juliettrowland@gmail.com>
2023-08-11 21:52:05 -0700

    Creation of arrays with ranging numbers

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * array_range - This create an array with range
+ * @min: Takes in interger argument
+ * @max: Takes in interger argument
+ * Return: return pointer on success and NULL if not successful
+ */
+int *array_range(int min, int max)
+{
+	int *range, i, size;
+
+	if (min > max)
+	return (NULL);
+
+	for (i = min; i < max; i++)
+	{
+		size++;
+	}
+
+	range = (int *) malloc(size * sizeof(int));
+	if (range == NULL)
+	return (NULL);
+
+	for (i = 0; min <= max; i++)
+	{
+		range[i] = min;
+		min++;
+	}
+	return (range);
+}
