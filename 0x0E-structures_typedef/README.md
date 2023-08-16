c1d3f70ae9d9e3acf7b6ede813234ef4fc04b990
JULESROWLAND <juliettrowland@gmail.com>
2023-08-15 18:57:37 -0700

    struct dog creation and intialization

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,6 @@
+dog.h
+1-init_dog.c
+2-print_dog.c
+Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read
+4-new_dog.c
+5-free_dog.c
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - This is a struct datatype
+ * @name: This takes a pointer to a character
+ * @age: This takes a float argument
+ * @owner: This takes a pointer to a character argument
+ */
+struct dog
+{
+	char  *name;
+	float age;
+	char *owner;
+};
+
+#endif
