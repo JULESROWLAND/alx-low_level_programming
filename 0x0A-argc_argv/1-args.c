b515267966b50b9ea9033f4c79af833d3d5fc876
JULESROWLAND <juliettrowland@gmail.com>
2023-08-19 21:29:26 -0700

    This is a count of argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is an entry point
+ * @argc: Take interger as argument to count
+ * @argv: Takes strings of argument
+ * Return: 0 always successful
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
